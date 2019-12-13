fba78d0484ed200dbe64ccbd9196c542a57f18dc
Calvin Feng <cfeng@fetchrobotics.com>
2019-12-12 21:10:16 -0800

    update data structure to be graph based

---
+++ README.md
@@ -0,0 +1,26 @@
+# JSON Schema
+
+## Sample
+
+```json
+{
+	"id": 1,
+	"edges": [
+		{ "id": 1, "src": 1, "dest": 4 },
+		{ "id": 1, "src": 2, "dest": 4 },
+		{ "id": 1, "src": 3, "dest": 5 },
+		{ "id": 1, "src": 4, "dest": 6 },
+		{ "id": 1, "src": 5, "dest": 6 },
+		{ "id": 1, "src": 5, "dest": 7 }
+	],
+	"nodes": [
+		{ "id": 1 },
+		{ "id": 2 },
+		{ "id": 3 },
+		{ "id": 4 },
+		{ "id": 5 },
+		{ "id": 6 },
+		{ "id": 7 }
+	]
+}
+```
--- main.go
+++ main.go
@@ -11,7 +11,7 @@ import (
 	"github.com/xeipuuv/gojsonschema"
 )
 
-var animalSchema gojsonschema.JSONLoader
+var graphSchema, nodeSchema, edgeShema gojsonschema.JSONLoader
 var log = logrus.New()
 
 func init() {
@@ -22,55 +22,63 @@ func init() {
 		log.Fatal(err)
 	}
 
-	animalSchema = gojsonschema.NewReferenceLoader(fmt.Sprintf("file://%s/schema/animal.schema.json", path))
+	graphSchema = gojsonschema.NewReferenceLoader(fmt.Sprintf("file://%s/schema/graph.schema.json", path))
+	nodeSchema = gojsonschema.NewReferenceLoader(fmt.Sprintf("file://%s/schema/node.schema.json", path))
+	edgeShema = gojsonschema.NewReferenceLoader(fmt.Sprintf("file://%s/schema/edge.schema.json", path))
+}
+
+func validateGraphJSONBody(body json.RawMessage) error {
+	loader :=  gojsonschema.NewBytesLoader(body)
+	result, err := gojsonschema.Validate(graphSchema, loader)
+	if err != nil {
+		return err
+	}
+
+	if !result.Valid() {
+		ve := ValidationError{}
+		for _, err := range result.Errors() {
+			if err.Field() == gojsonschema.STRING_ROOT_SCHEMA_PROPERTY {
+				ve["graph"] = err.Description()
+			} else {
+				ve[err.Field()] = err.Description()
+			}
+		}
+
+		return ve
+	}
+
+	return nil
+}
+
+func graphPostHandler(ectx echo.Context) error {
+	var jsonBody json.RawMessage
+	if err := ectx.Bind(&jsonBody); err != nil {
+		return ectx.JSON(http.StatusBadRequest, HTTPErrorResponse{
+			Message: "failed to parse request data as JSON bytes",
+			Error:   err,
+		})
+	}
+
+	if err := validateGraphJSONBody(jsonBody); err != nil {
+		return ectx.JSON(http.StatusBadRequest, HTTPErrorResponse{
+			Message: "failed to pass validation",
+			Error:   err,
+		})
+	}
+
+	return ectx.JSON(http.StatusOK, HTTPSuccessResponse{
+		Message:     "directed acyclic graph is successfully created",
+		RequestBody: jsonBody,
+	})
 }
 
 func main() {
-
 	e := echo.New()
 	e.GET("/", func(ectx echo.Context) error {
 		return ectx.String(http.StatusOK, "Hello, World!")
 	})
 
 	api := e.Group("api/")
-	api.POST("animals/", func(ectx echo.Context) error {
-		var jsonBody json.RawMessage
-		if err := ectx.Bind(&jsonBody); err != nil {
-			return ectx.JSON(http.StatusBadRequest, HTTPErrorResponse{
-				Message: "failed to parse request data as JSON bytes",
-				Error:   err,
-			})
-		}
-
-		jsonLoader := gojsonschema.NewBytesLoader(jsonBody)
-		result, err := gojsonschema.Validate(animalSchema, jsonLoader)
-		if err != nil {
-			return ectx.JSON(http.StatusInternalServerError, HTTPErrorResponse{
-				Message: "failed to perform JSON validation",
-				Error:   err,
-			})
-		}
-
-		if !result.Valid() {
-			ve := ValidationError{}
-			for _, err := range result.Errors() {
-				if err.Field() == gojsonschema.STRING_ROOT_SCHEMA_PROPERTY {
-					ve["animal"] = err.Description()
-				} else {
-					ve[err.Field()] = err.Description()
-				}
-			}
-			return ectx.JSON(http.StatusBadRequest, HTTPErrorResponse{
-				Message: "data do not pass validation",
-				Error:   ve,
-			})
-		}
-
-		return ectx.JSON(http.StatusOK, HTTPSuccessResponse{
-			Message:     "animal is successfully created",
-			RequestBody: jsonBody,
-		})
-	})
-
+	api.POST("dags/", graphPostHandler)
 	e.Logger.Fatal(e.Start(":8000"))
 }
